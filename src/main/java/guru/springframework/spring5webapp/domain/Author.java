4c68c5882b69779d8cbd0f5e5582e543bb84816e
Zafar31 <38731131+Zafar31@users.noreply.github.com>
2020-10-26 01:23:10 +0500

    Adding author controller and view

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -25,6 +25,14 @@ public class Author {
         this.lastname = lastname;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getFirstname() {
         return firstname;
     }
